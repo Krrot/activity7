451a9a96fae60b5c69be05b428437358ea80c0e9
Krrot <angelrmaciel@hotmail.com>
2026-03-09 20:11:43 -0500

    Added seeders, course factory, populated database and updated README

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'cover',
+        'content',
+        'robotics_kit_id'
+    ];
+
     public function roboticsKit()
     {
         return $this->belongsTo(RoboticsKit::class);
