7992dcf9a03ae3636f243e98f9a72dc01d58dae0
Krrot <angelrmaciel@hotmail.com>
2026-03-09 19:26:52 -0500

    Completed activity

--- database/migrations/2026_03_09_235050_create_users_table.php
+++ database/migrations/2026_03_09_235050_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            
+            $table->enum('role',['student','teacher','admin']);
+
+            $table->foreignId('group_id')->nullable()->constrained('groups');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
